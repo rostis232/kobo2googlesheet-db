8f3aaf229ada9ec1a2c1c081983d1be72faf99b5
Rostyslav Pylypiv <rostislav.pylypiv@gmail.com>
2023-11-07 16:00:37 +0200

    Disabled internal logging

--- internal/app/logwriter/logwriter.go
+++ internal/app/logwriter/logwriter.go
@@ -3,39 +3,18 @@ package logwriter
 import (
 	"fmt"
 	"github.com/rostis232/kobo2googlesheet-db/config"
-	"os"
-	"time"
 )
 
 func WriteLogToFile(log any) error {
 	switch log.(type) {
 	case error:
-		if err := writeToFile(log); err != nil {
-			return err
-		}
+		fmt.Println(log)
 	case string:
 		if config.LogLevel == 0 {
-			if err := writeToFile(log); err != nil {
-				return err
-			}
+			fmt.Println(log)
 		}
 	default:
-		fmt.Println("unknown type of log")
+		fmt.Println("unknown type of log: ", log)
 	}
-	return nil
-}
-
-func writeToFile(log any) error {
-	logTime := time.Now()
-	fileName := logTime.Month().String()
-	file, err := os.OpenFile("./logs/"+fileName+".log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0777)
-	if err != nil {
-		return err
-	}
-	_, err = file.WriteString(logTime.Format(time.DateTime)+": "+fmt.Sprint(log))
-	if err != nil {
-		return err
-	}
-	file.Close()
 	return nil
 }
